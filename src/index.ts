549ec9855c992687440f69fb90dedc8ae2e60cbf
Kaveh Fereidouni <kaveh981@gmail.com>
2018-02-10 13:43:58 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,8 @@ createConnection().then(async connection => {
         newStoryPropertyName.propertyName = property.property;
         newStoryProperty.storyPropName = newStoryPropertyName;
         newStoryProperty.value = property.value;
-    /* if I comment out the line right bellow this comment the storyProperties won't be saved into database
+    /* if I comment out the line right bellow this comment the storyProperties won't be saved and returns an error saying:
+       ER_NO_DEFAULT_FOR_FIELD: Field 'storyStoryId' doesn't have a default value
        and if I uncomment it every thing will be saved into database but the return result has circular structure
        the desired beheaviour is that only one way inserting should be enough and not required to have this line */
         
